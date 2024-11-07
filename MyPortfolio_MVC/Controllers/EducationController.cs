1ace79059c6a7dbf3a4b8eab20a13db95031946d
can gündoğdu <gundogdu.can@hotmail.com>
2024-11-07 22:15:03 +0300

    07.11.2024

---
+++ MyPortfolio_MVC/Controllers/EducationController.cs
@@ -0,0 +1,29 @@
+﻿using MyPortfolio_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio_MVC.Controllers
+{
+    public class EducationController : Controller
+    {
+        MyPortfolioDbEntities db= new MyPortfolioDbEntities();
+
+        public ActionResult Index()
+        {
+            var values= db.TblEducations.ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteEducation(int id)
+        {
+            var value= db.TblEducations.Find(id);
+            db.TblEducations.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
